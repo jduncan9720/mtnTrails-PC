e7dc0595c9c20b039556a93b759c8e668656eda6
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-12-16 20:18:59 -0700

    deploying to heroku

--- public/js/index.js
+++ public/js/index.js
@@ -42,7 +42,7 @@ $('#newPainting').on("submit", function (event) {
     const paintingWidth = $('#paintingWidth').val()
     const paintingPrice = $('#paintingPrice').val()
     const paintingArtist = $('#paintingArtist').val()
-    const paintingFilename = $('#paintingFile').val()
+    const paintingFilename = $('#paintingFile').val().match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1]
 
     console.log(paintingName, paintingHeight, paintingWidth, paintingPrice, paintingArtist, paintingFilename)
 
@@ -60,6 +60,24 @@ $('#newPainting').on("submit", function (event) {
         success: console.log("Painting added"),
         dataType: "json"
     });
+
+    // const formData = new FormData();
+    // const fileField = document.querySelector('input[type="file"]');
+
+    // formData.append('username', 'abc123');
+    // formData.append('avatar', fileField.files[0]);
+
+    // fetch('https://example.com/profile/avatar', {
+    //     method: 'PUT',
+    //     body: formData
+    // })
+    //     .then(response => response.json())
+    //     .then(result => {
+    //         console.log('Success:', result);
+    //     })
+    //     .catch(error => {
+    //         console.error('Error:', error);
+    //     });
 
 });
 
